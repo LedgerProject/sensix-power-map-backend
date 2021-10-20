9333febc94caf03e131530c51b337ae283cda32e
Lucianovici <lucian.corduneanu@sensidev.com>
2021-10-20 22:57:31 +0300

    SIX-540 Use user_data field to get position coordinates

--- apps/geo/services/ingest_processed_payload.py
+++ apps/geo/services/ingest_processed_payload.py
@@ -22,8 +22,9 @@ class IngestProcessedPayloadService(object):
     def __init__(self, data: dict, **kwargs) -> None:
         self.device_eui = data['eui']
         self.device_type = data['type']
-        self.position = data['position']
         self.payload = data['payload']
+        self.user_data = data['user_data']
+        self.position = self.user_data.get('position', {})
         self.latitude = self.position.get('lat')
         self.longitude = self.position.get('lon')
         self.kwargs = kwargs
